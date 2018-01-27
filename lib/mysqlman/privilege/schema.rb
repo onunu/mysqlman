fd6dd02bae14ae0bdae622acd0f325b8c3ff4480
onunu <riku.onuma@livesense.co.jp>
2018-01-27 18:24:45 +0900

    add each privs model

--- lib/mysqlman/privilege/schema.rb
+++ lib/mysqlman/privilege/schema.rb
@@ -2,9 +2,9 @@ require 'mysqlman/connection'
 
 module Mysqlman
   module Privilege
-    class Global
-      TABLE = 'information_schema.USER_PRIVILEGES'
-      COLUMNS = {type: 'PRIVILEGE_TYPE', grant_option: 'IS_GRANTABLE'}
+    class Schema
+      TABLE = 'information_schema.SCHEMA_PRIVILEGES'
+      COLUMNS = {schema: 'TABLE_SCHEMA', type: 'PRIVILEGE_TYPE', grant_option: 'IS_GRANTABLE'}
 
       def initialize(user:)
         @user = user
@@ -13,7 +13,7 @@ module Mysqlman
       def all_privileges
         conn = Connection.new
         privileges = conn.query("SELECT #{COLUMNS.values.join(',')} FROM #{TABLE} WHERE GRANTEE = '\\\'#{@user.user}\\\'@\\\'#{@user.host}\\\''")
-        privileges.to_a.map { |row| {type: row[COLUMNS[:type]], grant_option: row[COLUMNS[:grant_option]] == 'YES' ? true : false} }
+        privileges.to_a.map { |row| {schema: row[COLUMNS[:schema]], type: row[COLUMNS[:type]], grant_option: row[COLUMNS[:grant_option]] == 'YES' ? true : false} }
       end
     end
   end
