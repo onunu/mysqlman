f01dea19caad2db63ef9bd6071bbaf8b0c9f9134
onunu <riku.onuma@livesense.co.jp>
2018-01-28 22:39:17 +0900

    add roles methods

--- lib/mysqlman/role.rb
+++ lib/mysqlman/role.rb
@@ -6,9 +6,35 @@ module Mysqlman
       def all
         files = Dir.glob("#{ROLE_DIR}/*.yml")
         files.map do |file|
-          YAML.load_file(file)
+          self.new(YAML.load_file(file))
         end
       end
+    end
+
+    attr_reader :name, :config
+
+    def initialize(config)
+      @name = config.keys.first
+      @config = config.values.first
+    end
+
+    def build_grants_statements(user:, host:)
+      [
+        build_global_grants_statements(user, host),
+        build_schema_grants_statements(user, host),
+        build_table_grants_statements(user, host)
+      ]
+    end
+
+    private
+
+    def build_global_grants_statements(user:, host:)
+    end
+
+    def build_schema_grants_statements(user:, host:)
+    end
+
+    def build_table_grants_statements(user:, host:)
     end
   end
 end
